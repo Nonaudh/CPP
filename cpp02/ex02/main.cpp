ceb71fe14eb3a3b23ccd3528afdf8b52ef57a98a
Arnaud Huge <ahuge@3C4.42angouleme.fr>
2024-12-03 13:29:38 +0100

    quick fix

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -1,7 +1,7 @@
 #include "Fixed.hpp"
 
 int main(void)
-	{
+{
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 	std::cout << a << std::endl;
@@ -10,6 +10,8 @@ int main(void)
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+	std::cout << b * Fixed(2) << std::endl;
+	std::cout << b / Fixed(2) << std::endl;
 	std::cout << Fixed::max( a, b ) << std::endl;
 	return 0;
 }
--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -5,9 +5,11 @@ int	main (void)
 {
 	ScavTrap Tmp("Michel blanc");
 	ScavTrap Mich = Tmp;
+	ClapTrap Test = Tmp;
 
-	Mich.guardGate();
-	Mich.beRepaired(100);
-	Mich.takeDamage(100);
-	Mich.takeDamage(100);
+	// Mich.guardGate();
+	// Mich.beRepaired(100);
+	// Mich.takeDamage(100);
+	// Mich.takeDamage(100);
+	Test.attack("Casper");
 }
